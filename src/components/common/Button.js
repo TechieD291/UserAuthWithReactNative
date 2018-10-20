1043ab64ff2aef2772e57352769737bfc26a30c3
DhruvaMehta <dhruvamehta22@gmail.com>
2018-10-21 02:44:26 +0530

    Log out functianality added

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -15,17 +15,6 @@ const Button = ({onPress, children}) => {
 };
 
 const styles = {
-    buttonStyle: {
-        flex: 1,
-        alignSelf: 'stretch',
-        backgroundColor: '#fff',
-        borderWidth: 1,
-        borderRadius: 5,
-        borderColor: '#007aff',
-        marginLeft: 5,
-        marginRight: 5
-    },
-
     textStyle: {
         alignSelf: 'center',
         color: '#007aff',
@@ -33,6 +22,17 @@ const styles = {
         fontWeight: '600',
         paddingTop: 10,
         paddingBottom: 10
+    },
+
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
     }
 }
 
